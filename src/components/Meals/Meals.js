db1d03e974305d3fc4d18c710ffd40118e22d07c
jaydenhash02 <jaydenxian@outlook.com>
2023-01-13 17:45:45 +1100

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+import React from "react";
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </React.Fragment>
+  )
+};
+
+export default Meals;
